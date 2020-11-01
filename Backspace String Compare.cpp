b5c835c5bc8d6629047d1fdfc4f791863c5038ee
Ryuk07 <rohit.edcjss@gmail.com>
2020-11-01 16:18:31 +0530

    backspace string compare

---
+++ Backspace String Compare.cpp
@@ -0,0 +1,70 @@
+Task: Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+Note:
+
+    1 <= S.length <= 200
+    1 <= T.length <= 200
+    S and T only contain lowercase letters and '#' characters.
+
+Follow up:
+
+    Can you solve it in O(N) time and O(1) space?
+
+
+
+Solution:
+
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+        string a,b;
+        for(auto i:S){
+            if(i!='#'){
+                a+=i;
+            }
+            else{
+                if(a.size()!=0){
+                    a.pop_back();
+                }
+            }
+        }
+        for(auto j:T){
+            if(j!='#'){
+                b+=j;
+            }
+            else{
+                if(b.size()!=0){
+                    b.pop_back();
+                }
+            }
+        }
+        return (a==b);
+    }
+};
