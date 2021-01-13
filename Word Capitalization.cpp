00262b6d87789bd202f062c0d24bf4e555c75828
Ryuk07 <rohit.edcjss@gmail.com>
2021-01-13 18:09:32 +0530

    word capitalization updated code

--- Word Capitalization.cpp
+++ Word Capitalization.cpp
@@ -57,3 +57,26 @@ int main(){
 	return 0;
 }
 
+
+
+solution 2:
+
+
+#include <bits/stdc++.h>
+using namespace std;	
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+    string s;
+    cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s;
+	return 0;
+}
+
