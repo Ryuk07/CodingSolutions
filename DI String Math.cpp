b7beca40fbc5a57c8655af62ece517050e5a5966
Ryuk07 <rohit.edcjss@gmail.com>
2021-01-18 19:58:34 +0530

    di string match updated

--- DI String Math.cpp
+++ DI String Math.cpp
@@ -31,7 +31,7 @@ Note:
 
 
 
-Solution:
+Solution 1:
 
 
 class Solution {
@@ -54,5 +54,34 @@ public:
             }
         }
         return ret;
+    }
+};
+
+
+
+
+Solution 2:
+
+
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int inc=0,dec=s.length();
+        vector<int> v;
+        for(int i=0;i<=s.length();i++)
+        {
+            if(s[i]=='I')
+            {
+                v.push_back(inc);
+                inc++;
+            }
+            if(s[i]=='D')
+            {
+                v.push_back(dec);
+                dec--;
+            }
+        }
+        v.push_back(inc);
+        return v;
     }
 };
