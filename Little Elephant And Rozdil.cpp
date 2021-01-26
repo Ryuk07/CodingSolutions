edc45b07c2a15af5e1b3e15944e2e34c6fb6c4cb
Ryuk07 <rohit.edcjss@gmail.com>
2021-01-26 13:08:54 +0530

    little elephant and rozdil updated

--- Little Elephant And Rozdil.cpp
+++ Little Elephant And Rozdil.cpp
@@ -86,3 +86,49 @@ int main(){
     return 0;
 }
 
+
+
+
+Solution(Little Modification Same Approach):
+
+
+
+#include <bits/stdc++.h>
+using namespace std;	
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+    
+    int n, time, min_time = 1000000001, min_index=0, count=1;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin>>time;
+        if (time < min_time)
+        {
+            min_time = time;
+            min_index = i;
+            count = 1;
+        }
+        else if (time == min_time)
+        {
+            count += 1;
+        }
+    }
+    if (count == 1)
+    {
+        cout<<min_index<<endl;
+    }
+    else
+    {
+        cout<<"Still Rozdil"<<endl;
+    }
+    return 0;
+}
+
