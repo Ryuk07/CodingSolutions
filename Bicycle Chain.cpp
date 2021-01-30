655a8b387e777c9eb591b8fb9f1b7acfb4480ee3
Ryuk07 <rohit.edcjss@gmail.com>
2021-01-30 20:29:49 +0530

    bicycle chain updated

--- Bicycle Chain.cpp
+++ Bicycle Chain.cpp
@@ -89,3 +89,45 @@ int main(){
     return 0;
 }
 
+
+
+Solution(Better Approach):
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int m;
+    cin >> m;
+    int b[m];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    int max = 0, count = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (b[j] % a[i] == 0)
+            {
+                if (max == b[j] / a[i])
+                {
+                    count++;
+                }
+                else if (max < b[j] / a[i])
+                {
+                    max = b[j] / a[i];
+                    count = 1;
+                }
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
