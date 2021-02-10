4c1494ac30cae4d805a69a5f1da30adefee3fbc9
Ryuk07 <rohit.edcjss@gmail.com>
2021-02-10 19:59:09 +0530

    highest divisor

---
+++ Highest Divisor.cpp
@@ -0,0 +1,75 @@
+Task: You are given an integer N. Find the largest integer between 1 and 10 (inclusive) which divides N
+
+.
+Input
+
+The first and only line of the input contains a single integer N
+
+.
+Output
+
+Print a single line containing one integer ― the largest divisor of N
+between 1 and 10
+
+.
+Constraints
+
+    2≤N≤1,000
+
+Subtasks
+
+Subtask #1 (100 points): original constraints
+Example Input 1
+
+24
+
+Example Output 1
+
+8
+
+Explanation
+
+The divisors of 24
+are 1,2,3,4,6,8,12,24, out of which 1,2,3,4,6,8 are in the range [1,10]. Therefore, the answer is max(1,2,3,4,6,8)=8
+
+.
+Example Input 2
+
+91
+
+Example Output 2
+
+7
+
+Explanation
+
+The divisors of 91
+are 1,7,13,91, out of which only 1 and 7 are in the range [1,10]. Therefore, the answer is max(1,7)=7
+
+
+
+Solution:
+
+
+#include <bits/stdc++.h>
+using namespace std;	
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+    long long n,i;
+    cin>>n;
+    for(i=10;i>=1;i--){
+    	if(n%i==0){
+    		break;
+    	}
+    }
+    cout<<i<<endl;
+    return 0;
+}
+
