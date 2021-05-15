cfd5d57cfbf22941a2afd4bfcfacb860352fed04
Ryuk07 <rohit.edcjss@gmail.com>
2021-05-15 12:11:14 +0530

    kadane's algorithm

--- Kadane's Algorithm.cpp
+++ Kadane's Algorithm.cpp
@@ -47,46 +47,22 @@ Constraints:
 Solution:
 
 
-Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
 
- 
 
-Example 1:
-
-Input:
-N = 5
-arr[] = {1,2,3,-2,5}
-Output:
-9
-Explanation:
-Max subarray sum is 9
-of elements (1, 2, 3, -2, 5) which 
-is a contiguous subarray.
-
-Example 2:
-
-Input:
-N = 4
-arr[] = {-1,-2,-3,-4}
-Output:
--1
-Explanation:
-Max subarray sum is -1 
-of element (-1)
-
- 
-
-Your Task:
-You don't need to read input or print anything. The task is to complete the function maxSubarraySum() which takes arr and N as input parameters and returns the sum of subarray with maximum sum.
-
- 
-
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(1)
-
- 
-
-Constraints:
-1 ≤ N ≤ 106
--107 ≤ A[i] <= 107
-
+int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int max_so_far = INT_MIN, max_ending_here = 0;
+        for(int i=0;i<n;i++){
+            max_ending_here+=arr[i];
+            if(max_so_far<max_ending_here){
+                max_so_far=max_ending_here;
+            }
+            if(max_ending_here<0){
+                max_ending_here=0;
+            }
+        }
+        return max_so_far;
+        
+    }
+};
