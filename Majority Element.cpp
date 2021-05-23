60ed62bba92eb9c688ee9eeec645cf2c3e3baefa
Ryuk07 <rohit.edcjss@gmail.com>
2021-05-23 19:14:11 +0530

    majority element updated

--- Majority Element.cpp
+++ Majority Element.cpp
@@ -49,3 +49,31 @@ public:
         return ans;
     }
 };
+
+
+
+
+Solution(O(n) and O(1) space):
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& v) {
+        int n=v.size();
+        int majority=v[0];
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            if(v[i]==majority){
+                cnt+=1;
+            }
+            else{
+                cnt-=1;
+                if(cnt==0){
+                    majority=v[i];
+                    cnt=1;
+                }
+            }
+        }
+        return majority;
+    }
+};
